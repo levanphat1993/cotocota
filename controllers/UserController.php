eeb748685d997f1e7c2d3461d748aa17ee586579
DESKTOP-6GUG76T\Amdin <levanphatit1993@gmail.com>
2022-08-11 21:48:50 +0700

    add folder template

---
+++ controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+use app\models\LoginForm;
+
+class UserController extends Controller
+{
+   /**
+     * Login action.
+     *
+     * @return Response|string
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+}
